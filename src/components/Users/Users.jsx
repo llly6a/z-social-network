6213b2161c5f2312977874a68c64fd74849b7be6
zotchik <zotchik@voltep.ru>
2020-10-31 12:59:55 +0300

    add get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,13 @@
+import * as axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
-
+import userPhoto from '../../assets/images/user-alt-512.png';
 let Users = (props) => {
 
     if(props.users.length === 0){
-    props.setUsers([
-        { id: 0, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: false, fullName: 'Mikhail', location: { country: 'Russia', city: 'Moscow' }, status: 'iam leraning React' },
-        { id: 1, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: true, fullName: 'Evgenia', location: { country: 'Russia', city: 'Moscow' }, status: '222' },
-        { id: 2, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: false, fullName: 'Neriida', location: { country: 'Russia', city: 'Moscow' }, status: '33' },
-        { id: 3, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: true, fullName: 'Mr. Gru', location: { country: 'Russia', city: 'Moscow' }, status: '4444' },
-        { id: 4, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: false, fullName: 'Kolos', location: { country: 'Russia', city: 'Moscow' }, status: '55555' },
-        { id: 5, photoUrl: 'https://v1.popcornnews.ru/avatars/QD4rjq.jpg', followed: true, fullName: 'Kostusha', location: { country: 'Russia', city: 'Moscow' }, status: '66666' }
-    ]);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items);
+        })
     }
 
     return (
@@ -19,7 +15,7 @@ let Users = (props) => {
             {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={s.userPhoto} alt='avatar'/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.userPhoto} alt='avatar'/>
                     </div>
                     <div>
                         {u.followed ?
@@ -29,12 +25,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)}
